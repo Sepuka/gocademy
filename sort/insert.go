37a4659c6fc4ee7ca989aacd404c77bad543ee6d
Maksim Shlomin <maksim.shlomin@iqoption.com>
2018-12-11 11:07:23 +0300

    простая сортировка

--- sort/insert.go
+++ sort/insert.go
@@ -1,5 +1,6 @@
 package sort
 
+// Пример наивной реализации устойчивой сортировки
 func dummy(in []int) []int {
 	for pos, el := range in {
 		j := pos
@@ -8,6 +9,36 @@ func dummy(in []int) []int {
 			j--
 		}
 		in[j] = el
+	}
+
+	return in
+}
+
+// Улучшение состоит в бинарном поиске подходящей позиции для нового элемента
+func improvedDummy(in []int) []int {
+	var left, right, med, j int
+
+	for pos, el := range in {
+		if pos == 0 {
+			continue
+		}
+		left = 0
+		right = pos
+
+		for left < right {
+			med = (left + right) / 2
+			if in[med] <= el {
+				left = med + 1
+			} else {
+				right = med
+			}
+		}
+
+		for j = pos; j >= right+1; j-- {
+			in[j] = in[j-1]
+		}
+
+		in[right] = el
 	}
 
 	return in
--- sort/insert_test.go
+++ sort/insert_test.go
@@ -1,24 +1,42 @@
 package sort
 
 import (
-	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
 type dataSet struct {
-	data []int
+	data     []int
 	expected []int
 }
 
-var dataProvider = []dataSet{
-	{[]int{0}, []int{0}},
-	{[]int{0,1}, []int{0,1}},
-	{[]int{1,0}, []int{0,1}},
-	{[]int{1,0,3,2,5,4}, []int{0,1,2,3,4,5}},
+type insertSortSuite struct {
+	suite.Suite
+	dataSet []dataSet
 }
 
-func TestDummy(t *testing.T) {
-	for _, set := range dataProvider {
-		assert.Equal(t, dummy(set.data), set.expected)
+func (suite *insertSortSuite) SetupTest() {
+	suite.dataSet = []dataSet{
+		{[]int{0}, []int{0}},
+		{[]int{0, 1}, []int{0, 1}},
+		{[]int{1, 0}, []int{0, 1}},
+		{[]int{1, 0, 3, 2, 5, 4}, []int{0, 1, 2, 3, 4, 5}},
 	}
+}
+
+func (suite insertSortSuite) TestDummy() {
+	for _, set := range suite.dataSet {
+		assert.Equal(suite.T(), dummy(set.data), set.expected)
+	}
+}
+
+func (suite insertSortSuite) TestImprovedDummy() {
+	for _, set := range suite.dataSet {
+		assert.Equal(suite.T(), improvedDummy(set.data), set.expected)
+	}
+}
+
+func TestInsertSort(t *testing.T) {
+	suite.Run(t, new(insertSortSuite))
 }
