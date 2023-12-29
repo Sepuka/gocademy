3f085779a1da57412173799eed7e75e41e38fdcd
Maksim Shlomin <m.shlomin@corp.vk.com>
2023-12-29 17:50:05 +0300

    remove double dublicates

--- arr/remove_duplicates.go
+++ arr/remove_duplicates.go
@@ -29,3 +29,43 @@ func removeDuplicates(nums []int) int {
 
 	return pos
 }
+
+func removeDuplicates2(nums []int) int {
+	if len(nums) < 3 {
+		return len(nums)
+	}
+
+	var pos = 0
+
+	for i, v := range nums {
+		if i < 2 {
+			continue
+		}
+		if v == nums[i-1] && v == nums[i-2] {
+			pos = i
+			break
+		}
+	}
+
+	if pos == 0 {
+		return len(nums)
+	}
+
+	var double = true
+
+	for _, v := range nums[pos:] {
+		if v != nums[pos-1] {
+			double = false
+			nums[pos] = v
+			pos++
+			continue
+		}
+		if v == nums[pos-1] && double == false {
+			double = true
+			nums[pos] = v
+			pos++
+		}
+	}
+
+	return pos
+}
--- arr/remove_duplicates_test.go
+++ arr/remove_duplicates_test.go
@@ -33,3 +33,35 @@ func Test_removeDuplicates(t *testing.T) {
 		assert.Equal(t, removeDuplicates(testCase.nums), testCase.expected)
 	}
 }
+
+func Test_removeDuplicates2(t *testing.T) {
+	var testCases = []struct {
+		nums     []int
+		expected int
+	}{
+		{
+			nums:     []int{1},
+			expected: 1,
+		},
+		{
+			nums:     []int{1, 1},
+			expected: 2,
+		},
+		{
+			nums:     []int{1, 1, 2},
+			expected: 3,
+		},
+		{
+			nums:     []int{1, 1, 1, 2, 2, 3},
+			expected: 5,
+		},
+		{
+			nums:     []int{0, 0, 1, 1, 1, 1, 2, 3, 3},
+			expected: 7,
+		},
+	}
+
+	for _, testCase := range testCases {
+		assert.Equal(t, removeDuplicates2(testCase.nums), testCase.expected)
+	}
+}
